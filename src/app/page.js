b4014d0c23c6af3c6f20fac0721f665853b164c8
svalenzuelaGraiph <svalenzuela@graiph.ai>
2024-10-27 01:49:47 -0300

    feat: Boton para subir pa arriba

--- src/app/page.js
+++ src/app/page.js
@@ -4,15 +4,18 @@ import Home from "@/components/Home";
 import Navbar2 from "@/components/Nabvar2";
 import Navbar from "@/components/Navbar";
 import Projects from "@/components/Projects";
+import ScrollToTopButtom from "@/components/ScrollToTopButtom";
 
 export default function Page() {
   return (
     <div>
       {/* Navbar fijo */}
+      {/* <Navbar/> */}
       <Navbar2 />
       {/* Secciones */}
       <Home />
       <Projects />
+      <ScrollToTopButtom/>
       <About />
       <Contact />
     </div>
