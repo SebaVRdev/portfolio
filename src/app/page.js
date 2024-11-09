7a9b7187fcb41c598e626f170f87395c35074e57
SebaVRdev <sebarm177@gmail.com>
2024-11-08 23:00:21 -0300

    fix: Corregir merge en main

--- src/app/page.js
+++ src/app/page.js
@@ -4,11 +4,7 @@ import Home from "@/components/Home";
 import Navbar2 from "@/components/Nabvar2";
 import Navbar from "@/components/Navbar";
 import Projects from "@/components/Projects";
-<<<<<<< HEAD
 import Skills from "@/components/Skills";
-=======
-import ScrollToTopButtom from "@/components/ScrollToTopButtom";
->>>>>>> b4014d0c23c6af3c6f20fac0721f665853b164c8
 
 export default function Page() {
   return (
@@ -20,7 +16,6 @@ export default function Page() {
       <Home />
       <Skills/>
       <Projects />
-      <ScrollToTopButtom/>
       <About />
       <Contact />
     </div>
